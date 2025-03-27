7c9c142b6c263414e4bf63a60bce0bc89b9854b2
alessandro <>
2025-03-27 12:56:57 +0100

    init skeleton animation

--- scenes/enemy/car.gd
+++ scenes/enemy/car.gd
@@ -1,8 +1,8 @@
 extends PathFollow2D
 
 var player_near: bool = false
-#@onready var line1: Line2D = $Turret/RayCast2D/Line2D
-#@onready var line2: Line2D = $Turret/RayCast2D2/Line2D
+@onready var line1: Line2D = $Turret/RayCast2D/Line2D
+@onready var line2: Line2D = $Turret/RayCast2D2/Line2D
 
 func fire():
 	Globals.health -= 20
